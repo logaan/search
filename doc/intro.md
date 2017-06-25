dc3b67b33357c9942cc6c03bb5250ba5127830f0
Logan Campbell <colin@logaan.net>
2017-06-25 11:42:23 +1000

    lein new search

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to search
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/search/core_test.clj
@@ -0,0 +1,7 @@
+(ns search.core-test
+  (:require [clojure.test :refer :all]
+            [search.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
