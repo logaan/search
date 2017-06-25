90747a3651594964880d814c13d78d5b98b5a541
Logan Campbell <colin@logaan.net>
2017-06-25 19:57:56 +1000

    40 char text fields

--- src/search/ui/text.clj
+++ src/search/ui/text.clj
@@ -1,6 +1,8 @@
 (ns search.ui.text
   (:require [lanterna.screen :as s]))
 
+(def field-length 40)
+
 (def text-field-opts
   {:styles #{:underline}})
 
@@ -18,18 +20,19 @@
     :backspace   [:set (remove-last input)]
     [:set (str input key)]))
 
-(defn last [input length]
+(defn last-n [input length]
   (let [end   (count input)
         start (max (- end length) 0)]
     (subs input start end)))
 
-(def field-length 19)
+(defn blank-str [length]
+  (apply str (take length (repeat " "))))
 
 (defn field [scr x y value focused?]
   (let [end-of-text  (+ x (count value))
         end-of-field (+ x field-length)
         cursor-x     (min end-of-text end-of-field)]
-    (s/put-string scr x y "                    " text-field-opts)
-    (s/put-string scr x y (last value field-length) text-field-opts)
+    (s/put-string scr x y (blank-str (inc field-length)) text-field-opts)
+    (s/put-string scr x y (last-n value field-length) text-field-opts)
     (if focused?
       (s/move-cursor scr cursor-x y))))
