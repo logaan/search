d96c96a8a1071590a62c1aaefb2bf0b72f10f4c1
Logan Campbell <colin@logaan.net>
2017-06-25 18:45:11 +1000

    three field ui with tab-index

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject search "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [cheshire "5.7.1"]
-                 [clj-time "0.13.0"]])
+                 [clj-time "0.13.0"]
+                 [clojure-lanterna "0.9.7"]])
---
+++ src/search/ui.clj
@@ -0,0 +1,80 @@
+(ns search.ui
+  (:require [lanterna.screen :as s]))
+
+;;- text field -----------------------------------------------------------------
+
+(def text-field-opts
+  {:styles #{:underline}})
+
+(defn text-input [input key]
+  (case key
+    :enter       [:next input]
+    :tab         [:next input]
+    :escape      [:exit input]
+    :reverse-tab [:prev input]
+    :backspace   [:set (subs input 0 (dec (count input)))]
+    [:set (str input key)]))
+
+(defn text-field [scr x y value focused?]
+  (let [text-start (inc x)]
+    (s/put-string scr x y "                    " text-field-opts)
+    (s/put-string scr text-start y value text-field-opts)
+    (if focused?
+      (s/move-cursor scr (+ text-start (count value)) y))))
+
+;;- specific drawing -----------------------------------------------------------
+
+(def fields
+  [:dataset :field :query :table])
+
+(defn draw [{:keys [dataset field query table index]} scr]
+  (let [focus (fields index)]
+    (doto scr
+     (s/clear)
+
+     (s/put-string 2 1 "Zendesk Search Coding Challenge" {:styles #{:bold}})
+
+     (s/put-string 2 3 "Dataset:")
+     (text-field 11 3 dataset (= :dataset focus))
+
+     (s/put-string 4 5 "Field:")
+     (text-field 11 5 field (= :field focus))
+
+     (s/put-string 4 7 "Query:")
+     (text-field 11 7 query (= :query focus))
+
+     (s/redraw))))
+
+;;- core -----------------------------------------------------------------------
+
+(def initial-state
+  {:dataset "Users"
+   :field   "_id"
+   :query   "72"
+   :table   {}
+   :index   0})
+
+(defn input-loop [scr]
+  (loop [state initial-state]
+    (draw state scr)
+    (let [focus          (fields (:index state))
+          focus-value    (state focus)
+          [action value] (text-input focus-value
+                                     (s/get-key-blocking scr))]
+      (if (= :exit action)
+        (println "exiting")
+        (recur
+         (case action
+           :set  (assoc state focus value)
+           :next (update-in state [:index] inc)
+           :prev (update-in state [:index] dec)))))))
+
+(defn start []
+  (let [scr (s/get-screen)]
+    (s/in-screen scr (input-loop scr))))
+
+(comment
+
+  (start)
+
+  )
