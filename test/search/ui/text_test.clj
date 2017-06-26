203154e1b315469513bd8bce259ce707bc7d4e73
Logan Campbell <colin@logaan.net>
2017-06-26 23:03:37 +1000

    more tests for ui text

--- test/search/ui/text_test.clj
+++ test/search/ui/text_test.clj
@@ -1,6 +1,6 @@
 (ns search.ui.text-test
   (:require [search.ui.text :as sut]
-            [clojure.test :refer [deftest is]]))
+            [clojure.test :refer [deftest is are]]))
 
 (deftest backspace-deletes-last
   (is (= [:set "fo"] (sut/input "foo" :backspace)))
@@ -9,3 +9,25 @@
 (deftest character-keys-get-appended
   (is (= [:set "foo"] (sut/input "fo" \o)))
   (is (= [:set "a"] (sut/input "" \a))))
+
+(deftest handles-input
+  (let [text "foo"]
+    (are [input result] (= result (sut/input text input))
+      :escape      [:exit text]
+      :enter       [:next text]
+      :tab         [:next text]
+      :down        [:next text]
+      :reverse-tab [:prev text]
+      :up          [:prev text]
+      :backspace   [:set "fo"]
+      \a           [:set "fooa"]
+      \z           [:set "fooz"]
+      :home        [:set text])))
+
+(deftest drops-start-of-strings
+  (is (= "oo" (sut/last-n "foo" 2)))
+  (is (= "" (sut/last-n "" 2)))
+  (is (= "foo" (sut/last-n "foo" 99))))
+
+(deftest blank-strings
+  (is (= "     " (sut/blank-str 5))))
