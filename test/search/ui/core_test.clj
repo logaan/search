117661adda7cbb778782d21032b3e2157f16a847
Logan Campbell <colin@logaan.net>
2017-06-26 23:18:06 +1000

    search test

--- test/search/ui/core_test.clj
+++ test/search/ui/core_test.clj
@@ -1,3 +1,15 @@
 (ns search.ui.core-test
   (:require [search.ui.core :as sut]
             [clojure.test :refer [deftest is are]]))
+
+(deftest search
+  (let [state {:dataset "users"
+               :field "age"
+               :query "20"
+               :data {"users" (delay
+                               [{"name" "foo" "age" 20}
+                                {"name" "bar" "age" 22}
+                                {"name" "baz" "age" 20}])}}]
+    (is (= [{"name" "foo" "age" 20}
+            {"name" "baz" "age" 20}]
+           (sut/search-results state)))))
