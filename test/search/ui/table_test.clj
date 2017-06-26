e418559499546ed57699692fcc4c4ca6a27323fd
Logan Campbell <colin@logaan.net>
2017-06-27 00:27:49 +1000

    updated arrow key scroll test

--- test/search/ui/table_test.clj
+++ test/search/ui/table_test.clj
@@ -3,15 +3,15 @@
             [clojure.test :refer [deftest is are]]))
 
 (deftest handles-input
-  (let [state {:selected 1 :expanded false}]
+  (let [state {:selected 1 :expanded false :scroll 1}]
     (are [input result] (= result (sut/input state input))
       :escape      [:exit state]
-      :enter       [:set {:selected 1 :expanded true}]
+      :enter       [:set {:selected 1 :expanded true :scroll 1}]
       :tab         [:next state]
       :reverse-tab [:prev state]
-      :up          [:set {:selected 0 :expanded false}]
-      \k           [:set {:selected 0 :expanded false}]
-      :down        [:set {:selected 2 :expanded false}]
-      \j           [:set {:selected 2 :expanded false}]
+      :up          [:set {:selected 1 :expanded false :scroll 0}]
+      \k           [:set {:selected 0 :expanded false :scroll 1}]
+      :down        [:set {:selected 1 :expanded false :scroll 2}]
+      \j           [:set {:selected 2 :expanded false :scroll 1}]
       \a           [:set state]
       \z           [:set state])))
