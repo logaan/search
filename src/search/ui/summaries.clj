c9748eb1a29ec4e2a6b067f4eab92b51133b58d7
Logan Campbell <colin@logaan.net>
2017-06-27 00:09:15 +1000

    fixed header widths

--- src/search/ui/summaries.clj
+++ src/search/ui/summaries.clj
@@ -3,8 +3,6 @@
             [search.repositories.common :as common]
             [clojure.string :as str]))
 
-(def f common/first-n)
-
 (defmulti header (fn [record] (:type (meta record))))
 
 (defmulti summarise (fn [record] (:type (meta record))))
@@ -24,32 +22,38 @@
     (apply (partial format pattern) trimmed)))
 
 ;;- user -----------------------------------------------------------------------
+(def user-widths [20 15 9 31])
+
 (defmethod header :user [_]
-  (format user-format "Name" "Alias" "Role" "Email"))
+  (column-summary ["Name" "Alias" "Role" "Email"] user-widths))
 
 (defmethod summarise :user [{:strs [name alias role email]}]
-  (column-summary [name alias role email] [20 15 9 31]))
+  (column-summary [name alias role email] user-widths))
 
 (defmethod detail :user [scr user]
   (detail-map scr user))
 
 ;;- ticket ---------------------------------------------------------------------
+(def ticket-widths [9 9 8 49])
+
 (defmethod header :ticket [_]
-  (format ticket-format "Type" "Priority" "Status" "Subject"))
+  (column-summary ["Type" "Priority" "Status" "Subject"] ticket-widths))
 
 (defmethod summarise :ticket [{:strs [type priority status subject]}]
-  (column-summary [type priority status subject] [9 9 8 49]))
+  (column-summary [type priority status subject] ticket-widths))
 
 (defmethod detail :ticket [scr ticket]
   (detail-map scr ticket))
 
 ;;- organization ---------------------------------------------------------------
+(def organization-widths [15 15 46])
+
 (defmethod header :organization [_]
-  (format organization-format "Name" "Details" "Domain Names"))
+  (column-summary ["Name" "Details" "Domain Names"] organization-widths))
 
 (defmethod summarise :organization [{:strs [name details domain_names]}]
   (let [joined-domains (str/join ", " domain_names)]
-    (column-summary [name details joined-domains] [15 15 46])))
+    (column-summary [name details joined-domains] organization-widths)))
 
 (defmethod detail :organization [scr organization]
   (detail-map scr organization))
